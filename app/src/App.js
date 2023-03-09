9b0c7c13ea8ac67c1fd087f708a04930cd431bed
Ishant Chauhan <ishantchauhan710@gmail.com>
2023-03-09 15:17:13 +0530

    SocketIo setup done

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,31 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ConnectionScreen from "./screens/connection/ConnectionScreen";
 import AppScreen from "./screens/app/AppScreen";
+import io from "socket.io-client";
 
 const App = () => {
   const callRef = useRef();
+
+  // Socket
+  useEffect(() => {
+    const socket = io("http://127.0.0.1:5000");
+
+    socket.on("connect", () => {
+      console.log("Socket connected");
+    });
+
+    socket.emit("join", "DATA: ishant");
+
+    socket.on("connect_error", (e) => {
+      console.log("Socket connection error, retrying..." + e);
+      setTimeout(() => socket.connect(), 5000);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected");
+    });
+  }, []);
 
   const router = createBrowserRouter([
     {
--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
-
 const ExpressPeerServer = require("peer").ExpressPeerServer;
-
+const socketIo = require("socket.io");
 const server = require("http").createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(
   "/peerjs",
@@ -11,5 +16,12 @@ app.use(
     debug: true,
   })
 );
+
+io.on("connection", function (socket) {
+  //console.log("Made socket connection");
+  socket.on("join", function (data) {
+    console.log("User joined " + data);
+  });
+});
 
 server.listen(5000, "127.0.0.1");
